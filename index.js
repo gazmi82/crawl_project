8bdaa26daa32966f23a39b1c0c693d008fa03a7b
gazmir <gazmir.sulcaj@epitech.eu>
2020-07-26 23:53:57 +0200

    crawl-e config

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
-const CrawlE = require('crawl-e/v0.5.0');
+const CrawlE = require('crawl-e')
 
 
 let crawlE = new CrawlE({
-	// ...
+	cinemas: [
+    {
+      name: 'Kino Gmunden',
+      address: 'Theatergasse 7, 4810 Gmunden',
+      website: 'http://www.kino-gmunden.at/',
+      phone: '0676 / 88 794 505'
+    }
+  ],
+
+  showtimes: {
+    url: 'http://www.kino-gmunden.at/?page_id=55&dt=:date:&page=-1',
+    urlDateFormat: 'DD-MM-YYYY',
+
+    movies: {
+      box: '.overview',
+      title: 'h1',
+
+      showtimes: {
+        box: '.time',
+        datetimeFormat: 'DD-MM-YYYY - HH:mm'
+      }
+    }
+  }
 });
 
-crawlE.crawl();
+crawlE.crawl()
